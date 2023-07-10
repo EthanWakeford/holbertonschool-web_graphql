d2cf4c478aa2efd265c85516c4047c4f8cc66920
EthanWakeford <ethanwakeford@gmail.com>
2023-07-10 12:04:17 -0500

    added task 3

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -17,6 +17,23 @@ const tasks = [
   },
 ];
 
+const projects = [
+  {
+    id: '1',
+    title: 'Advanced HTML',
+    weight: 1,
+    description:
+      'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!',
+  },
+  {
+    id: '2',
+    title: 'Bootstrap',
+    weight: 1,
+    description:
+      'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.',
+  },
+];
+
 const TaskType = new GraphQLObjectType({
   name: 'Task',
   fields: {
@@ -26,6 +43,16 @@ const TaskType = new GraphQLObjectType({
     description: { type: GraphQLString },
   },
 });
+
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: {
+    id: { type: GraphQLString },
+    weight: { type: GraphQLInt },
+    title: { type: GraphQLString },
+    description: { type: GraphQLString },
+  }
+})
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -39,6 +66,15 @@ const RootQuery = new GraphQLObjectType({
         return tasks.find((task) => task.id === args.id);
       },
     },
+    project: {
+      type: ProjectType,
+      args: {
+        id: {type: GraphQLString}
+      },
+      resolve: (parent, args) => {
+        return projects.find((project) => project.id === args.id);
+      }
+    }
   },
 });
 
